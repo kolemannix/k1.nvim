63a50f1a52d56446961d5ac120490b0bc7ebafe4
Koleman Nix <kolemannix@gmail.com>
2026-05-04 22:18:51 -0400

    reuse-client

--- lsp/k1.lua
+++ lsp/k1.lua
@@ -4,4 +4,7 @@ return {
   -- root_uri = vim.fn.getcwd(),
   root_markers = { 'main.k1', 'proj.k1', '.git' },
   cmd_env = { RUST_BACKTRACE = "1" },
+  reuse_client = function(client, config)
+    true
+  end
 }
