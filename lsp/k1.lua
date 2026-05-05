ecf5bbb6635e2b8167ded9d0990d1ace6f8ec949
Koleman Nix <kolemannix@gmail.com>
2026-05-04 22:03:41 -0400

    try root_markers

--- lsp/k1.lua
+++ lsp/k1.lua
@@ -1,6 +1,7 @@
 return {
-  filetypes = { 'k1' },
   cmd = { 'k1lsp' },
+  filetypes = { 'k1' },
+  root_markers = { '*.k1' },
   cmd_env = { K1_HOME = k1_home, RUST_BACKTRACE = "1" },
   root_dir = buffer_dir(bufnr),
 }
