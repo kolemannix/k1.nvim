efe3ef52bb38bb4616a5a828bbbbecf6b46d3d9c
Koleman Nix <kolemannix@gmail.com>
2026-05-04 22:11:11 -0400

    try root_markers

--- lsp/k1.lua
+++ lsp/k1.lua
@@ -1,6 +1,7 @@
 return {
   cmd = { 'k1lsp' },
   filetypes = { 'k1' },
-  root_markers = { '*.k1' },
-  cmd_env = { K1_HOME = k1_home, RUST_BACKTRACE = "1" },
+  -- root_uri = vim.fn.getcwd(),
+  root_markers = { 'main.k1', 'proj.k1', '.git' },
+  cmd_env = { RUST_BACKTRACE = "1" },
 }
