1780320f7a55bcd2368b5a6bb6fbb1667081d5ea
Koleman Nix <kolemannix@gmail.com>
2026-08-12 20:24:42 -0400

    proj.k1 module.k1

--- lsp/k1.lua
+++ lsp/k1.lua
@@ -2,7 +2,7 @@ return {
   cmd = { 'k1lsp' },
   filetypes = { 'k1' },
   -- root_uri = vim.fn.getcwd(),
-  root_markers = { 'main.k1', 'proj.k1', '.git' },
+  root_markers = { 'main.k1', 'module.k1', '.git' },
   cmd_env = { RUST_BACKTRACE = "1" },
   reuse_client = function(client, config)
     return true
