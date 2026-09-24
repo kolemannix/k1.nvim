9fd999a9452570a50ae2e7adf1b05b58bdeb1025
Koleman Nix <kolemannix@gmail.com>
2026-09-23 22:23:29 -0400

    full backtrace

--- lsp/k1.lua
+++ lsp/k1.lua
@@ -3,7 +3,7 @@ return {
   filetypes = { 'k1' },
   -- root_uri = vim.fn.getcwd(),
   root_markers = { 'main.k1', 'module.k1', '.git' },
-  cmd_env = { RUST_BACKTRACE = "FULL" },
+  cmd_env = { RUST_BACKTRACE = "full" },
   reuse_client = function(client, config)
     return true
   end
